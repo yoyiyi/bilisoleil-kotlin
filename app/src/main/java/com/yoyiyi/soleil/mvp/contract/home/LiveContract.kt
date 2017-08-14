d9534bf57cf47adc1ce82df2e30e88cbdf9ca950
zzq <90755>
2017-08-14 21:30:26 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/soleil/mvp/contract/home/LiveContract.kt
+++ app/src/main/java/com/yoyiyi/soleil/mvp/contract/home/LiveContract.kt
@@ -1,20 +1,16 @@
 package com.yoyiyi.soleil.mvp.contract.home
 
 import com.yoyiyi.soleil.base.BaseContract
-import com.yoyiyi.soleil.bean.live.LivePartition
 import com.yoyiyi.soleil.bean.live.MulLive
 
 /**
  * @author zzq  作者 E-mail:   soleilyoyiyi@gmail.com
- * *
+ *
  * @date 创建时间：2017/5/23 22:02
- * * 描述:
+ * 描述:
  */
 interface LiveContract {
     interface View : BaseContract.BaseView {
-      //   fun showLiveRecommend(liveRecommend: LiveRecommend)
-
-        // fun showLivePartition(livePartition: LivePartition)
 
         fun showMulLive(mulLives: List<MulLive>)
 
