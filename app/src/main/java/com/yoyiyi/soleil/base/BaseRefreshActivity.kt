b93e25275867d375b215373c7763d07e6bd369fe
zzq <2455676683@qq.com>
2017-08-15 16:48:42 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/soleil/base/BaseRefreshActivity.kt
+++ app/src/main/java/com/yoyiyi/soleil/base/BaseRefreshActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import com.yoyiyi.soleil.R
 import com.yoyiyi.soleil.utils.AppUtils
 import com.yoyiyi.soleil.widget.ProgressWheel
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import java.util.*
 
@@ -36,9 +35,9 @@ abstract class BaseRefreshActivity<T : BaseContract.BasePresenter<*>, K> : BaseA
 
 
     override fun initWidget() {
-        mRefresh = find<SwipeRefreshLayout>(R.id.refresh)
-        mRecycler = find<RecyclerView>(R.id.recycler)
-        mLoading = find<ProgressWheel>(R.id.pw_loading)
+        mRefresh = findViewById(R.id.refresh) as? SwipeRefreshLayout
+        mRecycler = findViewById(R.id.recycler) as? RecyclerView
+        mLoading = findViewById(R.id.pw_loading) as? ProgressWheel
         initRefreshLayout()
         initRecyclerView()
     }
