c7f7a706c05196f93d92b69c98b9f28faff1f803
zzq <2455676683@qq.com>
2017-08-17 09:17:43 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/soleil/adapter/home/live/LiveEntranceAdapter.kt
+++ app/src/main/java/com/yoyiyi/soleil/adapter/home/live/LiveEntranceAdapter.kt
@@ -7,9 +7,9 @@ import com.yoyiyi.soleil.bean.live.support.LiveEnter
 
 /**
  * @author zzq  作者 E-mail:   soleilyoyiyi@gmail.com
- * *
+ *
  * @date 创建时间：2017/5/23 23:30
- * * 描述:
+ * 描述:
  */
 class LiveEntranceAdapter(data: List<LiveEnter>?) : BaseQuickAdapter<LiveEnter, BaseViewHolder>(R.layout.item_live_entrance, data) {
 
